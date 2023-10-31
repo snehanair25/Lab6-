09a4f00f01ced23b477d5e3654cf23691ce7ea0b
Ephraim Nicolas <eppynicolas@eppys-MacBook-Air.local>
2023-10-31 19:20:10 -0400

    Added my own decoder because there was no main file present, and partner is unresponsive

---
+++ decoder.py
@@ -0,0 +1,8 @@
+#Ephraim Nicolas COP3502C
+def decode(new_password):
+#Ephraim Nicolas COP3502C
+    original_password = ""
+    for element in original_password:
+        nums = str(int(element) - 3)
+        original_password += nums
+    return original_password
