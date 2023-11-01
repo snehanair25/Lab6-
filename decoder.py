f58d2030833beb363deb45e017b0748c90aa4e4f
snehanair25 <149012435+snehanair25@users.noreply.github.com>
2023-11-01 15:59:42 -0400

    Delete decoder.py

--- decoder.py
+++
@@ -1,8 +0,0 @@
-#Ephraim Nicolas COP3502C
-def decode(new_password):
-#Ephraim Nicolas COP3502C
-    original_password = ""
-    for element in new_password:
-        nums = str(int(element) - 3)
-        original_password += nums
-    return original_password
